ca2fd4a24a0dad5265c37d77e2e9f32fd8a8ff4f
Ngoc Huong <Ngoc Huong@DESKTOP-33G4CSH>
2023-03-12 16:26:30 +0700

    Add project files.

---
+++ QLBH_HBC/Form1.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QLBH_HBC
+{
+    public partial class Form1 : DevExpress.XtraBars.FluentDesignSystem.FluentDesignForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void accordionControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void accordionControlElement6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void accordionControlElement11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
